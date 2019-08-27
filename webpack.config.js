b44885a42cf3c112616f77e0505a8f8584b312e0
Jack <mrzzcn@gmail.com>
2019-08-27 10:05:44 +0800

    :hammer: Webpack Configure

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,23 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const isDev = NODE_ENV === 'development';
+
+const publicPath = isDev ? '/' : '/dist/';
+
 module.exports = {
   entry: [
     'react-hot-loader/patch', //设置这里
     './src/index.js',
   ],
+  devServer: {
+    hot: true,
+    publicPath,
+  },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    publicPath,
     filename: 'bundle.js'
   },
   mode: 'development',
